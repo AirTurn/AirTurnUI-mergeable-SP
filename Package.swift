944e374168cb0e3f1913f4db68f8288d4acc1579
Nick Brook <nrbrook@gmail.com>
2023-06-30 14:49:48 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "4.8.0-b.1"
+let checksum = "77827b10e82d05447a9af35c1fb846b7fee2d31c24e6e821572347db1fa7f05f"
+
+let package = Package(
+    name: "AirTurnUI",
+    platforms: [.iOS(.v12)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "AirTurnUI",
+            targets: ["AirTurnUITargets"]
+        ),
+    ],
+    dependencies: [
+        .package(name: "AirTurnInterface",
+                         url: "https://github.com/AirTurn/AirTurnInterface-mergeable-SP.git",
+                         from: Version(version)!
+        )
+    ],
+    targets: [
+        .binaryTarget(name: "AirTurnUI", url: "https://dev.airturn.com/sdk/AirTurnUI.\(version).mergeable.spm.zip", checksum: checksum),
+        .target(name: "AirTurnUITargets",
+                        dependencies: [
+                            .target(name: "AirTurnUI"),
+                            .product(name: "AirTurnInterface", package: "AirTurnInterface")
+                        ],
+                        path: "Sources"
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# AirTurnUI
+
+AirTurnUI is a user interface for AirTurnInterface. See [https://dev.airturn.com/sdk](https://dev.airturn.com/sdk) for more info.
